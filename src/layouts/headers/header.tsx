c70d2b755f683dfbfc9f536ae76bb6ab6d3bef27
Adham Tarek <adhamtarek291@gmail.com>
2025-06-26 23:22:25 +0300

    fix header in mobile

--- src/layouts/headers/header.tsx
+++ src/layouts/headers/header.tsx
@@ -11,54 +11,63 @@ export default function Header() {
   const [openMenu, setOpenMenu] = useState(false)
 
   return (
-      <header id="navigation" className="header-2">
-        <div className="container">
-<div className="row justify-content-between">
-  {/* Logo Section */}
-  <div className="col-xl-2 col-lg-4 col-md-3 col-auto align-self-center">
-    <div className="site-logo">
-      <Link href="/">
-        <Image
-          src="/assets/img/logo-purp.svg"
-          alt={siteConfig.short_name}
-          width={150}
-          height={50}
-          priority
-        />
-      </Link>
-    </div>
-  </div>
+    <header id="navigation" className="header-2">
+      <div className="container">
+        <div className="row d-flex flex-nowrap justify-content-between">
+          {/* Logo Section */}
+          <div
+            className="col-auto p-0"
+            style={{
+              maxWidth: "34%",
+            }}
+          >
+            <div className="site-logo">
+              <Link href="/">
+                <Image
+                  className="h-auto"
+                  src="/assets/img/logo-purp.png"
+                  alt={siteConfig.short_name}
+                  width={150}
+                  height={50}
+                  priority
+                />
+              </Link>
+            </div>
+          </div>
 
-  {/* Main Navigation */}
-  <div className="col-xl-7 col-lg-7 col-md-7 col-6 menu_col justify-content-center d-flex align-self-center">
-    <nav id="main-menu">
-      <NavMenu />
-    </nav>
-  </div>
+          {/* Main Navigation */}
+          <div className="col-xl-7 col-lg-7 col-md-7 col-6 menu_col justify-content-center d-flex align-self-center flex-grow">
+            <nav id="main-menu">
+              <NavMenu />
+            </nav>
+          </div>
 
- {/* Call to Action Button */}
-<div className="col-xl-3 col-lg-3 col-md-3 col-auto align-self-center text-end me-5 me-sm-0">
-  <Link href="/dashboard/login" className="bg_btn bt">
-    Get Started
-  </Link>
-</div>
-</div>
+          {/* Call to Action Button */}
+          <div className="col-xl-3 col-lg-3 col-md-3 col-auto align-self-center text-end pe-5 pe-sm-0 flex-shrink-0">
+            <Link
+              href="/dashboard/login"
+              className="bg_btn call-action-button bt"
+            >
+              Get Started
+            </Link>
+          </div>
         </div>
+      </div>
 
-        {/* Mobile Menu Toggle */}
-        <div
-          id="sm_menu_ham"
-          className={openMenu ? "open" : ""}
-          onClick={() => setOpenMenu(!openMenu)}
-        >
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
+      {/* Mobile Menu Toggle */}
+      <div
+        id="sm_menu_ham"
+        className={openMenu ? "open" : ""}
+        onClick={() => setOpenMenu(!openMenu)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
 
-        {/* Mobile Menu Component */}
-        <MobileMenu opneMenu={openMenu} />
-      </header>
-  )
+      {/* Mobile Menu Component */}
+      <MobileMenu opneMenu={openMenu} />
+    </header>
+  );
 }
