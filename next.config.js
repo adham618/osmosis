a8d3a0c4397854118c39f41c97263816df85d2ff
Adham Tarek <adhamtarek291@gmail.com>
2025-06-26 22:34:16 +0300

    add header

---
+++ next.config.js
@@ -0,0 +1,14 @@
+import withPlugins from 'next-compose-plugins';
+import withSvgr from 'next-plugin-svgr';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  compiler: {
+    removeConsole:
+      process.env.NODE_ENV === 'production'
+        ? { exclude: ['error', 'warn'] }
+        : false,
+  },
+};
+
+export default withPlugins([withSvgr], nextConfig);
--- src/layouts/headers/menu_data.ts
+++ src/layouts/headers/menu_data.ts
@@ -1,71 +1,50 @@
 interface DataType {
-	id: number;
-	title: string;
-	link: string;
-	has_dropdown?: boolean;
-	sub_menus?: {
-		link: string;
-		title: string;
-	}[];
+  id: number;
+  title: string;
+  link: string;
+  has_dropdown?: boolean;
+  sub_menus?: {
+    link: string;
+    title: string;
+  }[];
 }
+
 // menu data
 const menu_data: DataType[] = [
-	{
-		id: 1,
-		title: "Home",
-		link: "#",
-		has_dropdown: true,
-		sub_menus: [
-			{ link: "/", title: "Home One" },
-			{ link: "/home-2", title: "Home Two" },
-		],
-	}, 
-	{
-		id: 2,
-		title: "Courses",
-		link: "#",
-		has_dropdown: true,
-		sub_menus: [
-			{ link: "/courses", title: "Course Style1" },
-			{ link: "/courses-2", title: "Course Style2" },
-			{ link: "/course-details", title: "Course Details" },
-		],
-	},
-
-	{
-		id: 3,
-		title: "Pages",
-		link: "#",
-		has_dropdown: true,
-		sub_menus: [
-			{ link: "/grid-blog", title: "Grid Blog" },
-			{ link: "/standard-blog", title: "Standard Blog" },
-			{ link: "/blog-details", title: "Blog Details" },
-			{ link: "/cart", title: "Cart" },
-			{ link: "/checkout", title: "Checkout" },
-			{ link: "/login", title: "Login" },
-			{ link: "/register", title: "Register" },
-			{ link: "/about", title: "About" },
-			{ link: "/instructors", title: "Instructors" },
-			{ link: "/erorr", title: "404 || Error" },
-		],
-	},
-	{
-		id: 4,
-		title: "Blog",
-		link: "#",
-		has_dropdown: true,
-		sub_menus: [
-			{ link: "/grid-blog", title: "Grid Blog" },
-			{ link: "/standard-blog", title: "Standard Blog" },
-			{ link: "/blog-details", title: "Blog Details" },
-		],
-	},
-	{
-		id: 5,
-		title: "Contact",
-		link: "/contact",
-		has_dropdown: false,
-	},
+  {
+    id: 1,
+    title: "Home",
+    link: "/",
+    has_dropdown: false,
+  },
+  {
+    id: 2,
+    title: "For Students",
+    link: "#",
+    has_dropdown: true,
+    sub_menus: [
+      { link: "/k12", title: "K-12" },
+      { link: "/special-needs", title: "Special Needs" },
+      { link: "/college-beyond", title: "College & Beyond" },
+      { link: "/courses-1", title: "Explore Courses" },
+    ],
+  },
+  {
+    id: 3,
+    title: "For Educators",
+    link: "#",
+    has_dropdown: true,
+    sub_menus: [
+      { link: "/educator-features", title: "For Instructors" },
+      { link: "/educator-pricing", title: "Pricing" },
+    ],
+  },
+  {
+    id: 4,
+    title: "Opportunities",
+    link: "#",
+    has_dropdown: false,
+  },
 ];
+
 export default menu_data;
