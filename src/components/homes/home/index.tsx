49627b79ed988e663575fca514ea87339d55f747
Adham Tarek <adhamtarek291@gmail.com>
2025-06-26 03:14:03 +0300

    first commit

---
+++ src/components/homes/home/index.tsx
@@ -0,0 +1,38 @@
+
+
+import HeaderOne from '@/layouts/headers/HeaderOne'
+import React from 'react'
+import HeroHomeOne from './HeroHomeOne'
+import FeatureHomeOne from './FeatureHomeOne'
+import AboutHomeOne from './AboutHomeOne'
+import CounterHomeOne from './CounterHomeOne'
+import CoursesHomeOne from './CoursesHomeOne'
+import CourseCategoryHomeOne from './CourseCategoryHomeOne'
+import WorkingProcessHomeOne from './WorkingProcessHomeOne'
+import InstructorsHomeOne from './InstructorsHomeOne'
+import VideoHomeOne from './VideoHomeOne'
+import ReviewHomeOne from './ReviewHomeOne'
+import BrandHomeOne from './BrandHomeOne'
+import BlogHomeOne from './BlogHomeOne'
+import FooterOne from '@/layouts/footers/FooterOne'
+
+export default function HomeOne() {
+  return (
+    <>
+      <HeaderOne />
+      <HeroHomeOne />
+      <FeatureHomeOne />
+      <AboutHomeOne />
+      <CounterHomeOne />
+      <CoursesHomeOne />
+      <CourseCategoryHomeOne />
+      <WorkingProcessHomeOne />
+      <InstructorsHomeOne />
+      <VideoHomeOne />
+      <ReviewHomeOne />
+      <BrandHomeOne />
+      <BlogHomeOne />
+      <FooterOne />
+    </>
+  )
+}
