3f47cbd7b1df585ded5e0d5efd316c16bbde21f7
Adham Tarek <adhamtarek291@gmail.com>
2025-06-28 06:00:29 +0300

    forgot password

--- src/app/dashboard/(auth)/forgot-password/forgot-password.tsx
+++ src/app/dashboard/(auth)/forgot-password/forgot-password.tsx
@@ -1,5 +1,59 @@
+'use client'
+
 import * as React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function ForgotPassword() {
-  return <div></div>
+  const handleResetSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    // Add your reset password logic here
+    console.log('Reset password form submitted')
+  }
+
+  return (
+    <section className="page-section main login-page">
+      <div className="full-width-screen">
+        <div className="container-fluid p-0">
+          <div className="water-bg" id="water-js"></div>
+          <div className="content-detail">
+            {/* Forgot form */}
+            <form className="forgot-form" method="post" onSubmit={handleResetSubmit}>
+              <div className="imgcontainer">
+                <Image
+                  src="/assets/img/logo-purp.svg"
+                  alt="Logo"
+                  className="avatar logo-avatar-2"
+                  width={100}
+                  height={100}
+                  priority
+                />
+              </div>
+              <div className="input-control">
+                <p>Enter your email, we will send a link to reset your password.</p>
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  name="email"
+                  id="reset-email"
+                  required
+                />
+                <div className="login-btns">
+                  <button type="submit">Reset</button>
+                </div>
+                <div className="login-with-btns">
+                  <span className="already-acc">
+                    Return to{' '}
+                    <Link href="/login" className="login-btn">
+                      Login
+                    </Link>
+                  </span>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
