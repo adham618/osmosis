b852758edca010c456f60ec24f2385d1a6add6f5
Adham Tarek <adhamtarek291@gmail.com>
2025-06-29 01:24:22 +0300

    add ScrollToTop

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,5 +1,11 @@
+import ScrollToTop from '@/components/common/ScrollToTop'
 import Layout from '@/layouts/layout'
 
 export default function PubLicLayout({ children }: { children: React.ReactNode }) {
-  return <Layout>{children}</Layout>
+  return (
+    <Layout>
+      {children}
+      <ScrollToTop />
+    </Layout>
+  )
 }
