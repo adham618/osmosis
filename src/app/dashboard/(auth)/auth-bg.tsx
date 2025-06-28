d61fb8539bd6a9a44710b02e7fa4547d966ffab6
Adham Tarek <adhamtarek291@gmail.com>
2025-06-28 05:15:07 +0300

    fix blank page

--- src/app/dashboard/(auth)/auth-bg.tsx
+++ src/app/dashboard/(auth)/auth-bg.tsx
@@ -1,10 +1,19 @@
 'use client'
 
 import dynamic from 'next/dynamic'
+import { useEffect, useState } from 'react'
 
 const WaterWave = dynamic(() => import('react-water-wave'), { ssr: false }) as any
 
 export function AuthBg({ children }: { children: React.ReactNode }) {
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setLoaded(true), 1000)
+
+    return () => clearTimeout(timeout)
+  }, [])
+
   return (
     <WaterWave
       imageUrl="/assets/img/background-1.jpg"
@@ -20,12 +29,18 @@ export function AuthBg({ children }: { children: React.ReactNode }) {
         backgroundPosition: 'center'
       }}
     >
-      {() => (
-        <>
-          <div className="position-absolute top-0 bottom-0 start-0 end-0 bg-black bg-opacity-50 z-0"></div>
-          <div className="position-relative z-10 w-100 h-100">{children}</div>
-        </>
-      )}
+      {() =>
+        loaded ? (
+          <>
+            <div className="position-absolute top-0 bottom-0 start-0 end-0 bg-black bg-opacity-50 z-0"></div>
+            <div className="position-relative z-10 w-100 h-100">{children}</div>
+          </>
+        ) : (
+          <div className="loader-wrapper" id="loader-wrapper">
+            <div className="loader"></div>
+          </div>
+        )
+      }
     </WaterWave>
   )
 }
